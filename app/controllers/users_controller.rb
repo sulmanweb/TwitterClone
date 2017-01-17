59739aaef2f61b13aee0e9179aa619a70f0256f2
Sulman Baig <sulmanweb@gmail.com>
2017-01-18 00:07:08 +0500

    Adding user lists and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_user_by_current_user, only: [:edit_profile]
+  before_action :set_user_by_id, only: [:show]
 
   def edit_profile
     if @user.update(edit_profile_params)
@@ -10,6 +11,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def index
+    # TODO add pagination
+    if user_signed_in?
+      @users = User.where.not(id: current_user.id).all.order('id DESC')
+    else
+      @users = User.all.order('id DESC')
+    end
+  end
+
+  def show
+
+  end
+
   private
   def edit_profile_params
     params.permit(:name, :username, :bio, :location, :website, :dob)
@@ -17,5 +31,9 @@ class UsersController < ApplicationController
 
   def set_user_by_current_user
     @user = current_user
+  end
+
+  def set_user_by_id
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
 
     # Users
-    resources :users, only: [] do
+    resources :users, only: [:index, :show] do
       collection do
         put :edit_profile
       end
